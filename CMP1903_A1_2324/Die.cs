3a6eef3fbd6c125359a335d2690407758af7490c
kam-s8 <158074517+kam-s8@users.noreply.github.com>
2024-03-06 12:19:49 +0000

    Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -11,13 +11,15 @@ namespace CMP1903_A1_2324
         /*
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
-         */
+        */
+        class Die:
+    def __init__(self):
+        //Initialize the current value of the die
+        self.current_value = 0
 
-        //Property
-
-
-        //Method
-
-
+    def roll(self):
+        //Roll the die and update its current value
+        self.current_value = random.randint(1, 6)
+        return self.current_value
     }
 }
