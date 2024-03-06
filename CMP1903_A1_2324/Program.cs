baab0386de8d291cdf7dfa7dadc7168da0f738b2
kam-s8 <158074517+kam-s8@users.noreply.github.com>
2024-03-06 16:11:20 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -14,6 +14,7 @@ namespace CMP1903_A1_2324
              * Create a Game object and call its methods.
              * Create a Testing object to verify the output and operation of the other classes.
              */
+            Testing.testDie();
         }
     }
 }
