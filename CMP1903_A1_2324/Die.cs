e524745eb8c8b38ee4aeb92bde4685bc3da27a67
kam-s8 <158074517+kam-s8@users.noreply.github.com>
2024-03-07 12:22:18 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -12,11 +12,11 @@ namespace CMP1903_A1_2324
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
         */
-    static void __init__(self):
+    static __init__(self):
         //Initialize the current value of the die
         self.current_value = 0
 
-    static void roll(self):
+    static  roll(self):
         //Roll the die and update its current value
         self.current_value = random.randint(1, 6)
         return self.current_value
