c07ce0718e6732ea70f8c1138278ea6c2160ba9a
kam-s8 <158074517+kam-s8@users.noreply.github.com>
2024-04-29 14:09:15 +0100

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -12,33 +12,11 @@ namespace CMP1903_A1_2324
          * The Die class should contain one property to hold the current die value,
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
-	//Create the die object property with a value so it can be assigned a value
 	public int Value{get; set;}
         public Die(int value)
         {
             Value = value;
         }
-	public Dice();
-	 {
-		public int total1 = 0;
-		public int total2 = 0;
-	 }
-	//method
-	public static int Roll()
-        {
-		int roll1 = random.Next(1,7);
-		int roll2 = random.Next(1,7);
-		total1 += roll1;
-		total2 += roll2;
-
-		Console.WriteLine($"Current Roll: {roll1}, Total : {total1}");
-		Console.WriteLine($"Current Roll: {roll2}, Total : {total2}");
-
-		if (roll1 == 7 || roll2 == 7)
-		{
-			Console.WriteLine("One of the dice has landed on 7, stop rolling.");
-		}
-
-        }
+	
 	
 }
