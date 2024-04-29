9255ee52990c4abf2681208b902e23808db57c05
kam-s8 <158074517+kam-s8@users.noreply.github.com>
2024-04-29 10:57:31 +0100

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -18,16 +18,27 @@ namespace CMP1903_A1_2324
         {
             Value = value;
         }
-		
-		//method
-		//create a method that generates a random number 1-6 and it returns that value to anything that calls this method
-		public static int Roll()
+	publi Dice();
+	 {
+		total1 = 0;
+		total2 = 0;
+	 }
+	//method
+	public static int Roll()
         {
-			//Console.WriteLine("The code is still working.")
-			Random rnd = new Random();
-			//randomise the number
-			int dieNumber = rnd.Next(1, 7);
-			//return the number
-			return dieNumber;
+		int roll1 = random.Next(1,7);
+		int roll2 = random.Next(1,7);
+		total1 += roll1;
+		total2 += roll2;
+
+		Console.WriteLine($"Current Roll: {roll1}, Total : {total1}");
+		Console.WriteLine($"Current Roll: {roll2}, Total : {total2}");
+
+		if (roll1 == 7 || roll2 == 7)
+		{
+			Console.WriteLine("One of the dice has landed on 7, stop rolling.");
+		}
+
         }
+	
 }
