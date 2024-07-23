87d494f7e8b094aa45435686ab314e8d1dfb0fc9
kam-s8 <158074517+kam-s8@users.noreply.github.com>
2024-07-23 11:33:01 +0100

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -44,36 +44,7 @@ namespace CMP1903_A1_2324
                     break;
             }
                 
-            static void SevensOut()
-            {
-                Dice dice1 = new Dice();
-                Dice dice2 = new Dice();
-                int totalScore = 0;
-
-                while (true)
-                {
-                    int roll1 = dice1.Roll();
-                    int roll2 = dice2.Roll();
-                    int rollSum = roll1 + roll2;
-
-                    Console.WriteLine($"Roll: [{roll1}, {roll2}] (Sum={rollSum})");
-
-                    if (rollSum == 7)
-                    {
-                        Console.WriteLine($"End total = {totalScore}");
-                        break;
-                    }
-                    else if (roll1 == roll2) // Check if it's a double
-                    {
-                        totalScore += rollSum * 2;
-                    }
-                    else
-                    {
-                        totalScore += rollSum;
-                    }
-
-                    Console.WriteLine($"Current total = {totalScore}");
-                }
+            
         }
             
             Testing.testDie(); //check if dice are rolling as intended
